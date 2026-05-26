f65d954aceaaa741a5b4e123cee05477e968fe01
PyFlow Solutions <consultoria.pyflowsolutions@gmail.com>
2026-05-26 15:55:22 +0200

    Update FinderPageWrapper.tsx

--- src/components/FinderPageWrapper.tsx
+++ src/components/FinderPageWrapper.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import FilterSidebar from './FilterSidebar';
 import DistroResultsPanel from './DistroResultsPanel';
 import CompareBar from './CompareBar';
-import { useDistroFilter } from '@/hooks/useDistroFilter';
+import { useDistroFilter } from './useDistroFilter';
 import { distros as initialDistros } from './distroData';
 
 interface FinderPageWrapperProps {
